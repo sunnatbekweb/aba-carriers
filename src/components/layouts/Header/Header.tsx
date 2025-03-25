d8144cd7f64d70cfdccc39e324cab5b81d8b7c22
sunnatbekweb <sunnatbekkadamov@gmail.com>
2025-03-25 12:39:35 +0500

    header responsive

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -10,20 +10,26 @@ import Link from "next/link";
 export const Header = () => {
   return (
     <header className={`${styles.header} flex justify-between`}>
-      <div className="w-full flex items-center h-[135px]">
+      <div className="w-full flex items-center h-[65px] md:h-[95px] lg:h-[110px] xl:h-[135px]">
         {" "}
         <div className="absolute left-0 top-0">
           <div
-            className={`${styles.logo} w-[410px] flex flex-col items-center py-5`}
+            className={`${styles.logo} w-40 md:w-[230px] xl:w-[410px] h-fit flex flex-col items-center py-3 lg:py-5`}
           >
-            <div className="relative z-[1]">
-              <Image width={160} height={65} src="/icons/Logo.svg" alt="Logo" />
-              <p className="text-lg text-white">CARRIERS</p>
+            <div className="flex flex-col items-center">
+              <Image
+                width={160}
+                height={65}
+                src="/icons/Logo.svg"
+                className="w-[70px] md:w-[120px] xl:w-[160px] md:h-auto"
+                alt="Logo"
+              />
+              <p className="text-[8px] md:text-sm xl:text-lg text-white">CARRIERS</p>
             </div>
           </div>
         </div>
-        <div className="container flex justify-end">
-          <div className="flex items-center gap-x-10">
+        <div className="container px-5 flex justify-end">
+          <div className="flex items-center gap-x-5 md:gap-x-10">
             <Link href={"#"} className={`${styles.icon}`}>
               <WatsApp />
             </Link>
@@ -35,13 +41,13 @@ export const Header = () => {
               className={`${styles.icon} flex items-center text-white gap-x-2.5 text-2xl`}
             >
               <Phone />
-              <span>Call</span>
+              <span className="hidden md:block">Call</span>
             </Link>
             <button
               className={`${styles.icon} flex items-center gap-x-2.5 text-white text-2xl cursor-pointer`}
             >
               <Menu />
-              <span>Menu</span>
+              <span className="hidden md:block">Menu</span>
             </button>
           </div>
         </div>
