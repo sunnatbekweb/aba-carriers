7f4147bc888e010eecb50e45766213a42ff4d4e0
sunnatbekweb <sunnatbekkadamov@gmail.com>
2025-03-28 18:09:46 +0500

    finished navbar

--- src/components/ui/NavModal.tsx
+++ src/components/ui/NavModal.tsx
@@ -8,38 +8,45 @@ import React from "react";
 export const NavModal: React.FC<navModalProps> = ({isOpen, onClose}) => {
     const path = usePathname()
 
+    if (isOpen) {
+        document.body.style.overflowY = "hidden";
+    } else {
+        document.body.style.overflowY = "auto";
+    }
+
     return (
         <div onClick={onClose}
              className={`${isOpen ? "opacity-100 visible" : "opacity-0 collapse"} nav_modal`}>
-            <nav className={`${isOpen ? "scale-100" : "scale-75"} duration-300 flex items-center justify-center h-full brigendsExpanded`}>
-                <ul className={"flex flex-col items-center gap-y-[105px]"}>
-                    <li>
+            <nav
+                className={`${isOpen ? "scale-100" : "scale-75"} duration-300 flex items-center justify-center h-full brigendsExpanded`}>
+                <ul className={"flex flex-col items-center gap-y-6 md:gap-y-14 lg:gap-y-[105px] max-w-[725px] mx-auto px-5 lg:px-0"}>
+                    <li className={"flex justify-center"}>
                         <Link href={"/"}
-                              className={`text-5xl uppercase  ${path === "/" ? "text-[#ff0000]" : "text-white"}`}>
+                              className={`text-xl md:text-3xl lg:text-5xl uppercase text-center ${path === "/" ? "text-[#ff0000]" : "text-white"}`}>
                             Home
                         </Link>
                     </li>
-                    <li>
+                    <li className={"flex justify-center"}>
                         <Link href={"/why"}
-                              className={`text-5xl uppercase  ${path === "/why" ? "text-[#ff0000]" : "text-white"}`}>
+                              className={`text-xl md:text-3xl lg:text-5xl uppercase text-center ${path === "/why" ? "text-[#ff0000]" : "text-white"}`}>
                             WHY ABA CARRIERS
                         </Link>
                     </li>
-                    <li>
+                    <li className={"flex justify-center"}>
                         <Link href={"/drive"}
-                              className={`text-5xl uppercase  ${path === "/drive" ? "text-[#ff0000]" : "text-white"}`}>
+                              className={`text-xl md:text-3xl lg:text-5xl uppercase text-center ${path === "/drive" ? "text-[#ff0000]" : "text-white"}`}>
                             Drive for aba
                         </Link>
                     </li>
-                    <li>
+                    <li className={"flex justify-center"}>
                         <Link href={"/services"}
-                              className={`text-5xl uppercase  ${path === "/services" ? "text-[#ff0000]" : "text-white"}`}>
+                              className={`text-xl md:text-3xl lg:text-5xl uppercase text-center ${path === "/services" ? "text-[#ff0000]" : "text-white"}`}>
                             ABA SERVICES
                         </Link>
                     </li>
-                    <li>
+                    <li className={"flex justify-center"}>
                         <Link href={"/about"}
-                              className={`text-5xl uppercase  ${path === "/about" ? "text-[#ff0000]" : "text-white"}`}>
+                              className={`text-xl md:text-3xl lg:text-5xl uppercase text-center ${path === "/about" ? "text-[#ff0000]" : "text-white"}`}>
                             ABOUT ABA CARRIERS INC
                         </Link>
                     </li>
