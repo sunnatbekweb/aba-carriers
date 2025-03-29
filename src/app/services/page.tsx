e90fa2d97c5b30eab512097c649649f56d51e777
sunnatbekweb <sunnatbekkadamov@gmail.com>
2025-03-29 22:47:00 +0500

    fixed page names

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,4 +1,4 @@
-export default function Page() {
+export default function Services() {
     return (
         <div>
             <h2>ABA Services</h2>
--- src/app/why/page.tsx
+++ src/app/why/page.tsx
@@ -1,4 +1,4 @@
-export default function Page() {
+export default function Why() {
     return (
         <div>
             <h2>Why ABA</h2>
