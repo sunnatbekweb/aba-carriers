4a33e9f7adee664661d6505765b4c1a099a7d6ea
sunnatbekweb <sunnatbekkadamov@gmail.com>
2025-05-09 15:05:57 +0500

    added seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,36 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "ABA - CARRIERS",
-  description: "Developed by OneSystem",
+  title: "ABA Carriers",
+  description:
+    " Founded in 2021, ABA has rapidly grown into a trusted name in the trucking industry. With a strong focus on efficiency and reliability, we have successfully delivered tens of thousands of loads across the country.",
+  keywords:
+    "ABA, aba, ABA Carriers, trucks, Carriers, American truck, USA trucking, trucking in USA, hiring, turck job, world trucks",
+  authors: [{ name: "OneSystem" }],
+  robots: "index, follow",
+  openGraph: {
+    title: "ABA Carriers",
+    description:
+      "At ABA, we are dedicated to delivering freight safely, efficiently, and on time across the USA. With a growing fleet and a team of experienced professionals, we provide reliable trucking services tailored to your logistics needs",
+    url: "https://abacarriers.com",
+    siteName: "ABA Carriers",
+    images: [
+      {
+        url: "/section_bg.png",
+        width: 1200,
+        height: 630,
+        alt: " Founded in 2021, <span>ABA</span> has rapidly grown into a trusted name in the trucking industry.",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "ABA Carriers",
+    description:
+      "At ABA, we are dedicated to delivering freight safely, efficiently, and on time across the USA. With a growing fleet and a team of experienced professionals, we provide reliable trucking services tailored to your logistics needs",
+    images: ["/carriers_image.png"],
+  },
 };
 
 export default function RootLayout({
